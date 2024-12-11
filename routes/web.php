9849e41f4cf50845296a2b55e383c7a19c7fc795
manpits <manpits@gmail.com>
2024-12-11 10:54:55 +0800

    add route clear-cache

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,14 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KontakController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/clear-cache', function () {
+    Artisan::call('config:clear');
+    Artisan::call('route:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('view:clear');
+    return "Cache cleared successfully!";
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/', function () {
         return view('dashboard');
